5518b3c46cdc052b412e2107edbf7d3d6fea15a8
VoltaicC <70348826+VoltaicC@users.noreply.github.com>
2021-04-24 13:58:47 -0400

    Add files via upload

---
+++ STUDENT_TRIPLE_MAJOR.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 24, 2021 at 05:58 PM
+-- Server version: 10.3.25-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `S2021_cbass_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `STUDENT_TRIPLE_MAJOR`
+--
+
+CREATE TABLE `STUDENT_TRIPLE_MAJOR` (
+  `MAJOR_ID` varchar(50) NOT NULL,
+  `STUDENT_ID` varchar(50) NOT NULL,
+  `THIRD_MAJOR` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `STUDENT_TRIPLE_MAJOR`
+--
+ALTER TABLE `STUDENT_TRIPLE_MAJOR`
+  ADD PRIMARY KEY (`MAJOR_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
